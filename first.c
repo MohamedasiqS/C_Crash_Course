6123477aa5aeff68c40c51db295e9fc830f8a3f2
MohamedasiqS <asiq.mohamed@thundersoft.com>
2022-01-31 17:31:44 +0530

    .

--- first.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int i;
-	printf("2.modification occur\n");
-	for(i=0;i<5;i++)
-		printf("I= %d\n",i);
-	return 0;
-}
---
+++ status_31-01-2022/three.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("hello");
+	printf("practise");
+	return 0;
+}
