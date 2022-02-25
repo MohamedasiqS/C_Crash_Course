4ff0ceb1575d55d9af39a775a8662574ec6aa837
MohamedasiqS <asiq.mohamed@thundersoft.com>
2022-02-25 19:07:19 +0530

    25-02-2022 assignment submitted

--- 25_02/double_ptr.c
+++ 25_02/double_ptr.c
@@ -6,6 +6,7 @@ void my_printf(int **ptr)
 	int i;
 	for(i=0;i<SIZE;i++)
 		printf("ptr + %d = %d\n",i,*(*ptr+i));
+	printf("\n");
 	return ;
 }
 void my_scanf(int **ptr)
@@ -15,11 +16,30 @@ void my_scanf(int **ptr)
                 scanf("%d",*ptr+i);
 	return ;
 }
+void sort(int **ptr)
+{
+	int i,j,temp;
+	for(i=0;i<SIZE-1;i++)
+	{
+	   for(j=0;j<SIZE-i-1;j++)
+	   {
+		   if(*(*ptr+j) > *(*ptr+j+1))
+		    {
+		      temp      = *(*ptr+j);
+		      *(*ptr+j)   = *(*ptr+j+1);
+		      *(*ptr+j+1) = temp;
+		    } 
+	   }
+	}
+	return ;
+}
 int main(void)
 {
 	int *ptr=0;
 	ptr=(int *)malloc(SIZE*sizeof(int));
 	my_scanf(&ptr);
+	my_printf(&ptr);
+	sort(&ptr);
 	my_printf(&ptr);
 	free(ptr);
 	ptr=NULL;
